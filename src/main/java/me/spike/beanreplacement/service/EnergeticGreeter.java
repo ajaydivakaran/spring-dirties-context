32b27866e1297565fd6dddb5adce7cda14d15897
Ajay <ajaydi@thoughtworks.com>
2020-06-21 11:23:58 +0100

    Removed more noise

--- src/main/java/me/spike/beanreplacement/service/EnergeticGreeter.java
+++ src/main/java/me/spike/beanreplacement/service/EnergeticGreeter.java
@@ -1,13 +1,9 @@
 package me.spike.beanreplacement.service;
 
 import me.spike.beanreplacement.contract.Message;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("test")
-@Primary
 public class EnergeticGreeter {
 
     public Message welcome() {
